bb7a5e0f70f16661eb315f1e4ad6cdcd799bf908
TimbakTwo <124986834+TimbakTwo@users.noreply.github.com>
2023-03-11 21:22:54 +0800

    Add files via upload

---
+++ Readme.txt
@@ -0,0 +1,14 @@
+Welcome, and thank you for trying out my project
+
+My project is A Casino Simulation to a popular online sandbox Game Growtopia
+
+-So if you still don't know in Growtopia there is a community made casino which is not intended/made by 
+developers and it is marked illegal which is resulting to get your account at risk and lose your in game currency
+while playing casino
+
+-So if you just wanna try a similar casino to game Growtopia just for entertainment,
+try out this simulation and enjoy
+
+(How To Use)
+
+-
---
+++ cheat.js
@@ -0,0 +1,54 @@
+let cheatButtonNum = 0;
+function cheat() {
+	
+	cheatButtonNum++;
+	if (cheatButtonNum == 1) {
+	document.getElementById('submitCheat').style.display = "block";	
+	document.getElementById('main').style.display = "none";
+	document.getElementById('cheat').value = "return";
+	}
+	
+	if (cheatButtonNum == 2) {
+		cheatButtonNum = 0;
+	}
+	
+	if (cheatButtonNum == 0) {
+	document.getElementById('submitCheat').style.display = "none";	
+	document.getElementById('main').style.display = "block";
+	document.getElementById('cheat').value = "Edit your balance";
+	}
+}
+
+let wlEditValue = 0;
+let dlEditValue = 0;
+let bglEditValue = 0;
+
+function saveEdit() {
+	wlEditValue = parseInt(document.getElementById('wlCheatInput').value);
+	dlEditValue = parseInt(document.getElementById('dlCheatInput').value);
+	bglEditValue = parseInt(document.getElementById('bglCheatInput').value);
+	
+	if (isNaN(wlEditValue) || isNaN(dlEditValue) || isNaN(bglEditValue) ) {
+		alert('enter only number as edit value');
+		return;
+	}
+	
+	if (wlEditValue > 200 || dlEditValue > 200 || bglEditValue > 200) {
+		return;
+	}
+	
+	
+	
+	wlValue = wlValue + wlEditValue;
+	dlValue = dlValue + dlEditValue;
+	bglValue = bglValue + bglEditValue;
+	
+	if (wlValue > 200 || dlValue > 200 || bglValue > 200 || wlValue < 0 || dlValue < 0 || bglValue < 0) {
+		wlValue = wlValue - wlEditValue;
+		dlValue = dlValue - dlEditValue;
+		bglValue = bglValue - bglEditValue;
+		
+		alert('wl, dl or bgl value should not be above 200 or below 0');
+		return;
+	}
+}
